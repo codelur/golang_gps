e4724e558b692325559e4e9c33403bbff339c3a3
Sal <codelur@gmail.com>
2025-01-07 13:29:23 -0800

    adding constants

---
+++ utils/constants.go
@@ -0,0 +1,8 @@
+package utils
+
+const (
+	GoogleMapsApiKey = ""
+	OneStepGPSKey    = ""
+	SettingsFilePath = "user_settings.json"
+	Origin           = "https://osg-625361629214.us-central1.run.app"
+)
