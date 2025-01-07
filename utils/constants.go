328acf71249230c6fbe74866ff3c1c9f0f028a7e
Sal <codelur@gmail.com>
2025-01-07 13:02:23 -0800

    improving code

--- utils/constants.go
+++
@@ -1,8 +0,0 @@
-package utils
-
-const (
-	GoogleMapsApiKey = "AIzaSyAw7V0Fipnt_4DKEqDqWIIHNNtGaX_nksQ"
-	OneStepGPSKey    = "Ja7WeGvn3uOIfBumCZ5OERQnt2qz5jY3rY7yEe8sNY0"
-	SettingsFilePath = "user_settings.json"
-	Origin           = "https://osg-625361629214.us-central1.run.app"
-)
